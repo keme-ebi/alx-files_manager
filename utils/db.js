b3e922efbaa092753b3ce2799c54ce8729fb8e07
keme-ebi <kelvin6bolou@gmail.com>
2024-05-13 03:21:57 +0100

    created a class that creates a client to MongoDB

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.log(`Connection error: ${err}`);
+    });;
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collections = this.db.collection('users');
+    const count = await collections.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const collections = this.db.collection('files');
+    const count = await collections.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
