2db89a98513437acd6a39aacb310e93e351fa249
keme-ebi <kelvin6bolou@gmail.com>
2024-05-15 01:54:42 +0100

    created a new endpoint and a file that contains the new endpoint to create a new user in DB

---
+++ controllers/UsersController.js
@@ -0,0 +1,27 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+
+    try {
+      const user = dbClient.db.collection('users');
+      const userExists = await user.findOne({ email });
+      if (userExists) return res.status(400).json({ error: 'Already exists' });
+
+      const hashed = sha1(password);
+      const result = await user.insertOne({ email, password: hashed });
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+module.exports = UsersController;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/', router);
 
 app.listen(port, () => {
